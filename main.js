8ace9757331760e191062ac8f823b7e7f7620157
AkshayaAkshaj <87810890+AkshayaAkshaj@users.noreply.github.com>
2021-12-23 16:20:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function preload(){
+
+
+
+}
+function setup() {
+
+  canvas = createCanvas(300, 300);
+  
+  canvas.center();
+  
+  video = createCapture(VIDEO);
+  
+  video.hide();
+
+  classifier=ml5.imageClassifier('MobileNet' ,modelLoaded);
+
+}
+function modelLoaded(){
+  console.log("Model Loaded");
+}
+function draw(){
+image(video , 0,0,300,300);
+classifier.classify(video ,gotResult);
+  
+}
+var previousResult="";
+
+function gotResult(error ,results){
+  if(error){
+    console.error(error);
+}else {
+if((results[0].confidence > 0.5)&&(previousResult!=results[0].label)){
+  console.log(results);
+  previousResult=results[0].label;
+  var synth=window.SpeechSynthesis;
+  speak_data="Object detected is"+results[0].label;
+  var utterThis=new SpeechSynthesisUtterance(speak_data);
+  synth.speak(speak_data);
+
+  document.getElementById("result_object_name").innerHTML="Object : "+results[0].label;
+  document.getElementById("result_object_accuracy").innerHTML="Accuracy : "+results[0].confidence.toFixed(3);
+}
+}
+}
+
+
